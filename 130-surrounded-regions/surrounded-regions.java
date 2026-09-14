ef7c5d400168bbb0a139dd2e3b13a65c91f21a88
NarmadhaM2706 <m.nandhu2706@gmail.com>
2026-09-14 17:09:07 +0530

    Time: 2 ms (75.40%) | Memory: 47.4 MB (92.72%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.java
+++ 130-surrounded-regions/surrounded-regions.java
@@ -1,8 +1,7 @@
 class Solution {
-    
-    public void dfs(char[][] grid,int i,int j )
+    public void dfs(char[][] grid,int i,int j)
     {
-        if(i<0 || j<0 || i==grid.length || j== grid[0].length||grid[i][j] != 'O' )
+        if(i<0 || j<0 || i==grid.length || j==grid[0].length || grid[i][j] != 'O')
         {
             return;
         }
@@ -12,30 +11,25 @@ class Solution {
         dfs(grid,i,j-1);
         dfs(grid,i,j+1);
     }
-    public void solve(char[][] grid)
-    {
+    public void solve(char[][] grid) {
         int m=grid.length;
-        int  n=grid[0].length;
-        for(int i=0;i<m;i++)
-        {
-        
-           if(grid[i][0]=='O')
-           {
-            dfs(grid,i,0);
-           }
+        int n=grid[0].length;
+        for(int i=0;i<m;i++){
+            if(grid[i][0] == 'O')
+            {
+                dfs(grid,i,0);
+            }
             if(grid[i][n-1]=='O')
             {
                 dfs(grid,i,n-1);
             }
-
         }
         for(int j=0;j<n;j++)
         {
-            if(grid[0][j] == 'O')
-            {
+            if(grid[0][j] == 'O'){
                 dfs(grid,0,j);
             }
-            if(grid[m-1][j]=='O')
+            if(grid[m-1][j] == 'O')
             {
                 dfs(grid,m-1,j);
             }
@@ -44,16 +38,15 @@ class Solution {
         {
             for(int j=0;j<n;j++)
             {
-                if(grid[i][j] == '2')
-                {
-                    grid[i][j]='O';
-                }
-                else if(grid[i][j]== 'O')
+                if(grid[i][j]=='O')
                 {
                     grid[i][j]='X';
                 }
+                if(grid[i][j]=='2')
+                {
+                    grid[i][j]='O';
+                }
             }
         }
-    
     }
 }
